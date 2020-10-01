4fddee8dbe76454869be7dcfcd354cd3c2059bc1
Mykola <nazimkoff@gmail.com>
2020-10-01 18:42:50 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0){
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0 ||array.length === 0){
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  let m = 0;
+  let result = 0;
+  if (arguments.length === 0 || array.length === 0){
+    return 0;
+  } else { m = array.reduce((a,b) => a+b, 0); result = m / array.length;}
+  return result;
 }
